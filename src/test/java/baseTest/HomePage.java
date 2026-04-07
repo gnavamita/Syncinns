70d649120f07adf8aa8c88bd047e8be7265de14c
navamita ghosh <gnavamita@gmail.com>
2026-04-07 17:54:13 +0530

    adding one more..

--- src/test/java/baseTest/HomePage.java
+++ src/test/java/baseTest/HomePage.java
@@ -23,6 +23,7 @@ public class HomePage {
         driver.manage().window().maximize();
     }
 
+    @Test
     public void Request_A_Demo() throws InterruptedException {
         driver.findElement(By.xpath("//span[normalize-space()='Request a Demo']")).click();
         driver.findElement(By.id("nameModern")).sendKeys("tester");
@@ -42,8 +43,23 @@ public class HomePage {
         select.selectByValue("68381b13e00523079ccb3226");
         driver.findElement(By.id("propertyModern")).sendKeys("BusinessName");
         Thread.sleep(3000);
-        driver.findElement(By.xpath("//div[@class='datepicker-icon-wrapper']")).sendKeys("April 10,2026" );
+        driver.findElement(By.xpath("//input[@placeholder='Choose a date']")).sendKeys("April 10,2026");;
+//        Thread.sleep(3000);
+//        Calendar.click();
+//        Calendar.
+//        Calendar.sendKeys(Keys.ENTER);
 
+    }
+
+    @Test
+    public void Talk_TO_Us() {
+        driver.findElement(By.xpath("//span[text()='Talk to Us']")).click();
+        driver.findElement(By.name("first_name")).sendKeys("tester");
+        driver.findElement(By.name("last_name")).sendKeys("last name");
+        driver.findElement(By.name("work_email")).sendKeys("tester@yopmail.com");
+        driver.findElement(By.name("company_name")).sendKeys("company@yopmail.com");
+        driver.findElement(By.name("message")).sendKeys("This is a test!");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
     }
 
     @AfterMethod(enabled = false)

27750e54b31a3e86ba4d99fafd0a59342314ba24
navamita ghosh <gnavamita@gmail.com>
2026-04-08 15:02:05 +0530

    adding new file

--- src/test/java/baseTest/HomePage.java
+++ src/test/java/baseTest/HomePage.java
@@ -97,6 +97,7 @@ public class HomePage {
         driver.findElement(By.xpath("//div[text()='For Reservation Agencies']")).click();
     }
 
+    @Test
     public void Solutions_For_Travel_Companies() throws InterruptedException {
         Thread.sleep(2000);
         WebElement solutions = driver.findElement(By.xpath("//a[text()='Solutions']"));
@@ -107,6 +108,7 @@ public class HomePage {
         driver.findElement(By.xpath("//div[text()='For Travel Companies']")).click();
     }
 
+    @Test
     public void Solutions_For_B2B_Marketplace() throws InterruptedException {
         Thread.sleep(2000);
         WebElement solutions = driver.findElement(By.xpath("//a[text()='Solutions']"));
@@ -117,7 +119,49 @@ public class HomePage {
         driver.findElement(By.xpath("//div[text()='For B2B Marketplace']")).click();
     }
 
+    @Test
+    public void Pricing() {
+        driver.findElement(By.xpath("//a[text()='Pricing']")).click();
+        driver.findElement(By.xpath("//a[@href='https://syncinns.com/for-hotel-pricing']")).click();
+    }
 
+    @Test
+    public void Syncinns_AI() {
+        driver.findElement(By.xpath("//ul[@class='navbar-nav']//a[text()='Syncinns AI']")).click();
+        driver.findElement(By.xpath("//a[text()='Upgrade with AI']")).click();
+
+    }
+
+    @Test
+    public void Get_a_demo() throws InterruptedException {
+        try {
+            driver.findElement(By.xpath("//span[text()='Get a demo']")).click();
+            driver.findElement(By.id("nameModern")).sendKeys("tester");
+            driver.findElement(By.id("finalEmailModern")).sendKeys("tester@yopmail.com");
+            driver.findElement(By.xpath("//div[@class='iti__selected-country-primary']")).click();
+            Thread.sleep(3000);
+            WebElement flag = driver.findElement(By.xpath("//input[@placeholder='Search']"));
+            flag.sendKeys("india");
+            Thread.sleep(3000);
+            flag.sendKeys(Keys.ENTER);
+            driver.findElement(By.id("mobile_code")).sendKeys("9876543210");
+            WebElement solution = driver.findElement(By.id("employeeModern"));
+            solution.click();
+            Thread.sleep(3000);
+            Select select = new Select(solution);
+            Thread.sleep(3000);
+            select.selectByValue("68381b13e00523079ccb3226");
+            driver.findElement(By.id("propertyModern")).sendKeys("BusinessName");
+            Thread.sleep(3000);
+            WebElement calendar = driver.findElement(By.xpath("//input[@placeholder='Choose a date']"));
+            calendar.sendKeys("April 10,2026");
+        } catch (Exception e) {
+            System.out.println(e);;
+        }
+        finally {
+            System.out.println("form filled");
+        }
+    }
 
     @AfterMethod(enabled = false)
     public void close_browser() {

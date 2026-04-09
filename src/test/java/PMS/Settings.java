807ac4ccd438b8f30f015f17e5367b47c70da998
navamita ghosh <gnavamita@gmail.com>
2026-04-09 15:00:32 +0530

    adding second

--- src/test/java/PMS/Settings.java
+++ src/test/java/PMS/Settings.java
@@ -3,7 +3,9 @@ package PMS;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -24,7 +26,7 @@ public class Settings {
     }
 
     @Test
-    public void settingModule() throws InterruptedException{
+    public void settingModule() throws InterruptedException {
         Thread.sleep(4000);
         driver.findElement(By.xpath("//app-side-nav[@class='ng-star-inserted']//a[@href='/syncinns-pms/settings']")).click();
         Thread.sleep(4000);
@@ -39,5 +41,27 @@ public class Settings {
         driver.findElement(By.xpath("//span[normalize-space()='Operations']")).click();
         Thread.sleep(3000);
         driver.findElement(By.xpath("//div[@class='gs-acc-header']//span[text()='Account Settings']")).click();
+    }
+
+    @Test
+    public void hrmModule()throws InterruptedException{
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//a[@class='nav-link dropdown-toggle']//span[text()='HRM']")).click();
+        Thread.sleep(3000);
+       driver.findElement(By.xpath("//a[normalize-space()='Manage Employee']")).click();
+        Thread.sleep(3000);
+       driver.findElement(By.xpath("//a[normalize-space()='Manage Roster']")).click();
+        Thread.sleep(3000);
+       driver.findElement(By.xpath("//a[normalize-space()='Manual Attendance']")).click();
+        Thread.sleep(3000);
+       driver.findElement(By.xpath("//a[normalize-space()='Daily Attendance']")).click();
+        Thread.sleep(3000);
+       driver.findElement(By.xpath("//a[normalize-space()='Daily Attendance']")).click();
+        Thread.sleep(3000);
+       driver.findElement(By.xpath("//a[normalize-space()='Attendance Summary']")).click();
+        Thread.sleep(3000);
+       driver.findElement(By.xpath("//a[normalize-space()='Leaves']")).click();
+        Thread.sleep(3000);
+       driver.findElement(By.xpath("//a[normalize-space()='My Leaves']")).click();
     }
 }

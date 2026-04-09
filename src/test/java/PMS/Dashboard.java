349ba957797e7fac447311d8384f9f32e42b78f8
navamita ghosh <gnavamita@gmail.com>
2026-04-09 13:06:52 +0530

    adding dashboard program

--- src/test/java/PMS/Dashboard.java
+++ src/test/java/PMS/Dashboard.java
@@ -20,14 +20,28 @@ public class Dashboard {
         driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("pmstest@yopmail.com");
         driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("123456");
         driver.findElement(By.xpath("//button[@class='tpfLoginBtn']")).click();
-        Thread.sleep(2000);
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
     }
 
     @Test
-    public void Front_office(){
-        driver.findElement(By.xpath("//button[text()=' Check in & Check out ']")).click();
-        driver.findElement(By.xpath("//button[text()=' Check in & Check out ']")).click();
-        driver.findElement(By.xpath("//button[minimize-spacing()=' Front Office & Reservation Desk ']")).click();
+    public void tabMenuList() throws InterruptedException {
+        //driver.findElement(By.id("overview-tab")).click();
+
+        driver.findElement(By.id("check-tab")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[normalize-space()='Front Office & Reservation Desk']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//ul[@class='nav nav-tabs mb-0']//button[normalize-space()='House Keeping']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[normalize-space()='Accounts']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[normalize-space()='HR Dashboard']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[normalize-space()='Restaurant']")).click();
+        Thread.sleep(3000);
 
     }
+
+
 }
---
+++ src/test/java/PMS/Settings.java
@@ -0,0 +1,5 @@
+package PMS;
+
+public class Settings {
+
+}

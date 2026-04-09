7cb40ed4970442081c0a01ad7647d1e667ed1b4f
navamita ghosh <gnavamita@gmail.com>
2026-04-09 18:46:00 +0530

    adding last

--- src/test/java/PMS/Settings.java
+++ src/test/java/PMS/Settings.java
@@ -185,7 +185,58 @@ public class Settings {
         Thread.sleep(3000);
         driver.findElement(By.xpath("//span[text()='Reports']")).click();
         driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Advance Payment Report']")).click();
-        }
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Receivables Report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Revenue Leakage Report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Tax Summary Report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Reservation report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Arrival Report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='No show report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Cancelled Report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='occupancy report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and normalize-space()='Average daily rate (ADR Report)']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Daily RevPAR Breakdown']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='RevPAR by room Type']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Room Status Report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='In-House Guest List']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Checkout List']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Repeat guest Report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Early Check-in/Check-out Report']")).click();
+
+
+
+    }
 
 
 

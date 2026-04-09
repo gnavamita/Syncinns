4635951afae9cfa101a824d7b8e1e8b6ba0cf641
navamita ghosh <gnavamita@gmail.com>
2026-04-09 17:05:01 +0530

    adding new file

--- src/test/java/PMS/Settings.java
+++ src/test/java/PMS/Settings.java
@@ -5,9 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 @Test
 public class Settings {
@@ -88,11 +91,102 @@ public class Settings {
 
     @Test
     public void reservationsModule() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
         driver.findElement(By.xpath("//a[@class='nav-link dropdown-toggle']//span[text()='Reservations']")).click();
         driver.findElement(By.xpath("//a[normalize-space()='New Reservation']")).click();
         driver.findElement(By.xpath("//a[normalize-space()='Reservation List']")).click();
         driver.findElement(By.xpath("//a[normalize-space()='Check In']")).click();
         driver.findElement(By.xpath("//a[normalize-space()='Check Out']")).click();
     }
+
+    @Test
+    public void propertyModule() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[@class='nav-link dropdown-toggle']//span[text()='Property']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[normalize-space()='Show Properties']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[normalize-space()='Property Policies']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[normalize-space()='Add Room']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[normalize-space()='Manage Room Rate']")).click();
+    }
+
+    @Test
+    public void frontDeskModule() {
+        driver.findElement(By.xpath("//a[@class='nav-link dropdown-toggle']//span[text()='Front Desk']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Inventory & Stock']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Unavailability']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Room Status']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Check In']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Check Out']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Room Management']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='No Show Report']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Cancelled']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Lost and Found']")).click();
+    }
+
+    @Test
+    public void house_KeepingModule() {
+        driver.findElement(By.xpath("//a[@class='nav-link dropdown-toggle']//span[text()='Front Desk']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='House Insight']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Task List']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='User Engagement']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Maintenance List']")).click();
+    }
+
+    @Test
+    public void vendor_And_StockModule() {
+        driver.findElement(By.xpath("//a[@class='nav-link dropdown-toggle']//span[text()='Vendor & Stock']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Vendor']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Make Order']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Order List']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Purchase Entry']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Purchase List']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Stock Report']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Purchase Transaction']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Accounts']")).click();
+    }
+
+    @Test
+    public void RestaurantModule() {
+        driver.findElement(By.xpath("//a[@class='nav-link dropdown-toggle']//span[text()='Restaurant']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Create Table']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Table List']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Create Recipe']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Recipe List']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Create Order']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Buffet List']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Order List']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Transactions']")).click();
+    }
+
+    @Test
+    public void AccountsModule() {
+        driver.findElement(By.xpath("//a[@class='nav-link dropdown-toggle']//span[text()='Accounts']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Expenses']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Payment List']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='VAT Report']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Balance Sheet']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Hotel Transactions']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Restaurant Transactions']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Vendor Accounts']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Investments']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='All Transactions']")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Profit Reports']")).click();
+    }
+
+    @Test
+    public void reportModule() throws InterruptedException{
+        driver.findElement(By.xpath("//a[@href='/syncinns-pms/reports']//span[text()='Reports']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Daily Revenue Report']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//div[@class='fr-text']//div[@class='fr-title' and text()='Advance Payment Report']")).click();
+        }
+
+
 
 }

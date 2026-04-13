134941563ae5adf3de28fd5680ff1fc57617f570
gnavamita <gnavamita@gmail.com>
2026-04-13 22:14:40 +0530

    adding new files

--- src/main/java/Utilities/BaseClass.java
+++ src/main/java/Utilities/BaseClass.java
@@ -1,4 +1,24 @@
 package Utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseClass {
+
+    public WebDriver driver;
+
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    public void goToURL(String url) {
+        driver.get(url);
+    }
+
+    public void closeBrowser() {
+        driver.quit();
+    }
+
 }
--- src/main/java/Utilities/CommonFunctions.java
+++ src/main/java/Utilities/CommonFunctions.java
@@ -1,4 +1,25 @@
 package Utilities;
 
-public class CommonFunctions {
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+
+public class CommonFunctions extends BaseClass {
+
+    public void clickOnButton(String locator) {
+        driver.findElement(By.xpath(locator)).click();
+    }
+
+
+    public String fetchText(String locator) {
+        return driver.findElement(By.xpath(locator)).getText();
+    }
+
+    public void typeText(String locator, String text){
+        driver.findElement(By.xpath(locator)).sendKeys(text);
+    }
+
+    public void selectDropdown(String locator, String value){
+        Select select = new Select(driver.findElement(By.xpath(locator)));
+        select.selectByValue(value);
+    }
 }

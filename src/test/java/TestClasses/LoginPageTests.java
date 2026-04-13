cbf3bf22ead56d670bea1030eac9ebf551e4bcc5
gnavamita <gnavamita@gmail.com>
2026-04-13 23:19:42 +0530

    login page tests created

--- src/test/java/TestClasses/LoginPageTests.java
+++ src/test/java/TestClasses/LoginPageTests.java
@@ -9,38 +9,41 @@ import org.testng.annotations.Test;
 
 public class LoginPageTests {
 
-    BaseClass b = new BaseClass();
-    LoginPage l = new LoginPage();
-    DashboardPage d = new DashboardPage();
-    String SyncinnsURL = "https://app.syncinns.com/login";
 
-    @BeforeTest
-    public void goToURL() {
-        b.openBrowser();
-        b.goToURL(this.SyncinnsURL);
+        BaseClass b = new BaseClass();
+        LoginPage l = new LoginPage();
+        DashboardPage d = new DashboardPage();
+        String SyncinnsURL = "https://app.syncinns.com/login";
+
+        @BeforeTest
+        public void goToURL() {
+            b.openBrowser();
+            b.goToURL(this.SyncinnsURL);
+        }
+
+
+        @AfterTest
+        public void closeBrowser() {
+            b.closeBrowser();
+        }
+
+        @Test
+        public void user_able_to_login_with_correct_credentials() {
+            l.initialVerification();
+            l.enterUserName("pmstest@yopmail.com");
+            l.enterPassword("123456");
+            l.clickOnLoginButton();
+            d.verifyDashboardPageURL();
+        }
+
+        @Test
+        public void user_is_unable_to_login_with_incorrect_credentials() {
+            l.initialVerification();
+            l.enterUserName("");
+            l.enterPassword("");
+            l.clickOnLoginButton();
+            l.verifyErrorsOnLoginPage();
+        }
     }
 
 
-    @AfterTest
-    public void closeBrowser() {
-        b.closeBrowser();
-    }
-
-    @Test
-    public void user_able_to_login_with_correct_credentials() {
-        l.initialVerification();
-        l.enterUserName("pmstest@yopmail.com");
-        l.enterPassword("123456");
-        l.clickOnLoginButton();
-        d.verifyDashboardPageURL();
-    }
-
-    @Test
-    public void user_is_unable_to_login_with_incorrect_credentials() {
-        l.initialVerification();
-        l.enterUserName("");
-        l.enterPassword("");
-        l.clickOnLoginButton();
-        l.verifyErrorsOnLoginPage();
-    }
-}
